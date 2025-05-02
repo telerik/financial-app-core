d543cab647974568d43e496a4b396d3535e372f0
alestoya <alestoya@progress.com>
2025-05-02 18:07:01 +0300

    chore: introduce seeder logic

---
+++ Financial-App-Core/Extensions/Partials/SeederExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Financial_App_Core.Models;
+
+namespace Financial_App_Core.Extensions.Partials
+{
+    public static class SeederExtensions
+    {
+        public static List<DrawerItemPartialModel> CreateDrawerItem(
+            this List<DrawerItemPartialModel> container,
+            string displayName = null,
+            string id = null,
+            string url = null,
+            bool isSeparator = false)
+        {
+            container.Add(new DrawerItemPartialModel
+            {
+                DisplayName = displayName,
+                Id = id,
+                Url = url,
+                IsSeparator = isSeparator
+            });
+
+            return container;
+        }
+
+        public static List<MoverPartialModel> CreateMoverItem(
+            this List<MoverPartialModel> container,
+            string imageUrl,
+            string name,
+            string abbreviation,
+            TrendModel q1,
+            TrendModel q2,
+            decimal totalEarnings
+        )
+        {
+            container.Add(new MoverPartialModel
+            {
+                ImageUrl = imageUrl,
+                Abbreviation = abbreviation,
+                Name = name,
+                Q1 = q1,
+                Q2 = q2,
+                TotalEarnings = totalEarnings
+            });
+
+            return container;
+        }
+    }
+}
---
+++ Financial-App-Core/Models/Partials/DrawerItemPartialModel.cs
@@ -0,0 +1,30 @@
+﻿using Kendo.Mvc.Extensions;
+
+namespace Financial_App_Core.Models
+{
+    public class DrawerItemPartialModel
+    {
+        public string DisplayName
+        {
+            get;
+            set;
+        }
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        public string Url
+        {
+            get;
+            set;
+        }
+
+        public bool IsSeparator
+        {
+            get;
+            set;
+        }
+    }
+}
